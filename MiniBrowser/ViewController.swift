035dac21e412f8b545082c38b666425f8cff01c1
JuDH <forever43591@gmail.com>
2018-01-18 22:12:18 +0900

    Initial commit

--- MiniBrowser/ViewController.swift
+++ MiniBrowser/ViewController.swift
@@ -7,13 +7,68 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
+    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var mainWebView: WKWebView!
+    @IBOutlet weak var spinningActivityIndicatorView: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let initialURL = "https://www.facebook.com"
+        let myURL = URL.init(string: initialURL)
+        let myRequest = URLRequest(url: myURL!)
+        mainWebView.load(myRequest)
+        urlTextField.text = initialURL
+        mainWebView.navigationDelegate = self
     }
+
+    @IBAction func bookMarkAction(_ sender: Any) {
+        let bookMarkURL = bookMarkSegmentedControl.titleForSegment(at: bookMarkSegmentedControl.selectedSegmentIndex)
+        let urlString = "https://www.\(bookMarkURL!).com"
+        mainWebView.load(URLRequest(url: URL(string: urlString)!))
+        urlTextField.text = urlString
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        var urlString = "\(urlTextField.text!)"
+        if !urlString.hasPrefix("https://") {
+            urlString = "https://\(urlTextField.text!)"
+        }
+        urlTextField.text = urlString
+        mainWebView.load(URLRequest(url: URL(string: urlString)!))
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        spinningActivityIndicatorView.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        spinningActivityIndicatorView.stopAnimating()
+    }
+    
+    @IBAction func goBackAction(_ sender: Any) {
+        mainWebView.goBack()
+    }
+    
+    @IBAction func goForwardAction(_ sender: Any) {
+        mainWebView.goForward()
+    }
+    
+    @IBAction func stopLoadingAction(_ sender: Any) {
+        mainWebView.stopLoading()
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+        mainWebView.reload()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
